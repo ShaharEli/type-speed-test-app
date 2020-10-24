ddcef47ba9ac2e812c11c9fd329b37f712c4123c
ShaharEli <shahar.e3@gmail.com>
2020-10-24 18:17:49 +0300

    initial

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import styled from "styled-components";
+import randomWords from "random-words";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <MainContainer>
+        <Title>Typing Speed Tdest</Title>
+        <Button title='get Rndomm' onPress={() => console.log(randomWords())} />
+      </MainContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MainContainer = styled.SafeAreaView`
+  background-color: #56bcd2;
+  text-align: center;
+  align-items: center;
+  text-align: center;
+  flex: 1;
+`;
+
+const Title = styled.Text`
+  font-size: 30px;
+  margin: 30px 0;
+`;
